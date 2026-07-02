af5e15999ab22b7dd9d52791325d924e4dd80a4c
Cristian <dignasuss@gmail.com>
2026-07-02 18:10:35 -0400

    Modificacion App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import Login from './components/Login';
+import Dashboard from './components/Dashboard';
 
 function App() {
   const [vistaActual, setVistaActual] = useState(() => {
@@ -18,33 +19,12 @@ function App() {
 
   return (
     <div className="app-container">
-      {vistaActual === 'login' && (
+     {vistaActual === 'login' && (
         <Login onLoginSuccess={irAlDashboard} />
       )}
 
       {vistaActual === 'dashboard' && (
-        <div style={{ padding: '3rem', textAlign: 'center', fontFamily: 'sans-serif' }}>
-           <h2 style={{ color: '#333333' }}>Panel de Control: ProjectHub</h2>
-           <p style={{ color: '#666666', marginTop: '1rem' }}>
-             Módulo de Dashboard de Proyectos en construcción.
-           </p>
-           
-           <button 
-             onClick={manejarCerrarSesion}
-             style={{ 
-               marginTop: '2rem', 
-               padding: '0.5rem 1rem', 
-               cursor: 'pointer',
-               backgroundColor: '#dc3545',
-               color: 'white',
-               border: 'none',
-               borderRadius: '5px',
-               fontWeight: 'bold'
-             }}
-           >
-             Cerrar Sesión
-           </button>
-        </div>
+        <Dashboard onLogout={manejarCerrarSesion} />
       )}
     </div>
   );
