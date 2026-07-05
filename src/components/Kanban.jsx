3c151305a862b47984c9daf7116fe7a1c0f44857
Sebastian Neira <neiragavilan@gmail.com>
2026-07-04 21:04:13 -0400

    feat: Kanban completado

--- src/components/Kanban.jsx
+++ src/components/Kanban.jsx
@@ -30,7 +30,7 @@ function Kanban() {
     const updatedColumns = { ...columns };
 
     updatedColumns[activeColumns].items.push({
-      id: Date.now.toString(),
+      id: Date.now().toString(),
       content: newTask,
     });
 
@@ -96,9 +96,85 @@ function Kanban() {
     <>
       <div
         className="p-6 w-full min-h-screen bg-gradient-to-b
-            from-zinc-900 to-zing-800 flex items-center justify-center"
-      ></div>
-      <div></div>
+            from-zinc-900 to-zinc-800 flex items-center justify-center"
+      >
+        <div className="flex items-center justify-center flex-col gap-4 w-full max-w-6xl">
+          <h1 className="text-6xl font-bold mb-8 text-transparent bg-clip-text bg-gradient-to-r from-yellow-400  via-amber-500 to-rose-400">
+            React Kanban Board
+          </h1>
+          <div className="mb-8 flex w-full max-w-lg shadow-lg rounded-lg overflow-hidden">
+            <input
+              type="text"
+              value={newTask}
+              onChange={(e) => setNewTask(e.target.value)}
+              placeholder="Agregar nueva tarea..."
+              className="flex-grow p-3 bg-zinc-700 text-white"
+              onKeyDown={(e) => e.key === "Enter" && addNewTask()}
+            ></input>
+            <select
+              value={activeColumns}
+              onChange={(e) => setActiveColumns(e.target.value)}
+              className="p-3 bg-zinc-700 text-white border-0 border-l border-zinc-600"
+            >
+              {Object.keys(columns).map((columnId) => (
+                <option value={columnId} key={columnId}>
+                  {columns[columnId].nombre}
+                </option>
+              ))}
+            </select>
+
+            <button
+              onClick={addNewTask}
+              className="px-6 bg-gradient-to-r from-yellow-600 to-amber-500 text-white font-medium hover:from-yellow-500 hover:to-amber-500 transition-all duration-200 cursor-pointer"
+            >
+              Agregar
+            </button>
+          </div>
+          <div className="flex gap-6 overflow-x-auto pb-6 w-full">
+            {Object.keys(columns).map((columnId) => (
+              <div
+                key={columnId}
+                className={`flex-shrink-0 w-80 bg-zinc-800 rounded-lg shadow-x1 border-t-4 ${columnStyles[columnId.border]}`}
+                onDragOver={(e) => handleDragOver(e, columnId)}
+                onDrop={(e) => handleDrop(e, columnId)}
+              >
+                <div
+                  className={`p-4 text-white font-bold text-xl rounded-t-md ${columnStyles[columnId].header}`}
+                >
+                  {columns[columnId].nombre}
+                  <span className="ml-2 px-2 py-1 ">
+                    {columns[columnId].items.length}
+                  </span>
+                </div>
+                <div className="p-3 min-h-64">
+                  {columns[columnId].items.length === 0 ? (
+                    <div className="text-center py-10 text-zinc-500 italic text-sm ">
+                      Arrastra las tareas aquí
+                    </div>
+                  ) : (
+                    columns[columnId].items.map((item) => (
+                      <div
+                        key={item.id}
+                        className="p-4 mb-3 bg-zinc-700 text-white rounded-lg shadow-md cursor-move flex item-center justify-between transform transition-all duration-200 hover:scale-105 hover-shadow-lg"
+                        draggable
+                        onDragStart={() => handleDragStart(columnId, item)}
+                      >
+                        <span className="mr-2">{item.content}</span>
+                        <button
+                          onClick={() => removeTask(columnId, item.id)}
+                          className="text-zinc-400 hover: text-red-400 transition-colors duration-400 w-6 h-6 flex items-center justify-center rounded-full hover:bg-zinc-600"
+                        >
+                          <span className="text-lg  cursor-pointer">x</span>
+                        </button>
+                      </div>
+                    ))
+                  )}
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
     </>
   );
 }
